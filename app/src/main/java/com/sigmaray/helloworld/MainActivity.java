64b847bf745da07dfe387c7b44f83bf2d65928ee
sigmaray <sigmaray@gmail.com>
2016-04-10 00:31:07 +0300

    Added 2 buttons: first button shows message, second closes application

--- app/src/main/java/com/sigmaray/helloworld/MainActivity.java
+++ app/src/main/java/com/sigmaray/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.sigmaray.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.handleButtonClicks();
+    }
+
+    private void handleButtonClicks(){
+        Button buttonShowMessage = (Button) findViewById(R.id.buttonShowMessage);
+        buttonShowMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello, World!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button buttonClose = (Button) findViewById(R.id.buttonClose);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Is it correct way to exit app?
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
     }
 }
