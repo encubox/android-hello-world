06fd6c4837b4f82727827ab9fdd3c4a9a7be1dd6
sigmaray <sigmaray@gmail.com>
2016-04-12 21:14:15 +0300

    V2

--- app/src/main/java/com/sigmaray/helloworld/MainActivity.java
+++ app/src/main/java/com/sigmaray/helloworld/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         this.handleButtonClicks();
     }
 
-    private void handleButtonClicks(){
+    private void handleButtonClicks() {
         Button buttonShowMessage = (Button) findViewById(R.id.buttonShowMessage);
         buttonShowMessage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,9 +29,28 @@ public class MainActivity extends AppCompatActivity {
         buttonClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Is it correct way to exit app?
+                // Is it correct way to close app?
                 android.os.Process.killProcess(android.os.Process.myPid());
                 System.exit(1);
+            }
+        });
+
+        mp=MediaPlayer.create(this, R.raw.papua_dub);
+        mp.setLooping(true);
+        Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.mp.start();
+
+            }
+        });
+
+        Button buttonStop = (Button) findViewById(R.id.buttonStop);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.mp.pause();
             }
         });
     }
